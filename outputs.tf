f06881996aaceeca874d693e3f50bfc561ada95a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-09-04 09:29:59 +0200

    Use 0.12 syntax

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "private_ips" {
   description = "Private IP addresses of Zookeeper instances"
-  value       = [hsdp_container_host.zookeeper.*.private_ip]
+  value       = hsdp_container_host.zookeeper.*.private_ip
 }
 
 output "zookeeper_port" {
