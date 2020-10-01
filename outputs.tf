d7c0ce94d0dd48e0608bd56735664d20026e5b37
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-10-01 09:12:08 +0200

    Output names

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "zookeeper_nodes" {
   value       = hsdp_container_host.zookeeper.*.private_ip
 }
 
+output "zookeeper_name_nodes" {
+  description = "Container Host Zookeeper instance names"
+  value       = hsdp_container_host.zookeeper.*.name
+}
+
 output "zookeeper_port" {
   description = "Zookeeper port"
   value       = "10000"
