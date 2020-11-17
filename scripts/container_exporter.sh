7b770df2169e1a6a28a9139c578d11e253be7687
andriy.pentsak <andriy.pentsak@vitech.com.ua>
2020-11-17 19:13:16 +0200

    add merge export docker

--- scripts/container_exporter.sh
+++ scripts/container_exporter.sh
@@ -13,4 +13,4 @@ docker start zookeeper || echo "Zookeeper doesn't exist"
 docker run -d --name merge_exporter \
 -e MERGER_PORT=8888 \
 -e MERGER_URLS="http://`hostname`:9101/metrics http://`hostname`:9102/metrics http://`hostname`:9103" \
--p 10001:8888 apentsak/exporter-merger
+-p 10001:8888 quay.io/rebuy/exporter-merger
