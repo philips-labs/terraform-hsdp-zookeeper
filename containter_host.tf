30b48110d0b89fb6a8a9e99a88405dec764640fb
KP <krishna.prasad.srinivasan@philips.com>
2020-12-15 21:17:44 +0530

    add tld to the name

--- containter_host.tf
+++ containter_host.tf
@@ -4,7 +4,7 @@ resource "random_id" "id" {
 
 resource "hsdp_container_host" "zookeeper" {
   count         = var.nodes
-  name          = var.host_name == "" ? "zookeeper-${random_id.id.hex}-${count.index}.dev" : "zookeeper-${var.host_name}-${count.index}"
+  name          = var.host_name == "" ? "zookeeper-${random_id.id.hex}-${count.index}.dev" : "zookeeper-${var.host_name}-${count.index}.${var.tld}"
   volumes       = 1
   volume_size   = var.volume_size
   instance_type = var.instance_type
