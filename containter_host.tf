25a3a8d81d2fdbba390ab3332d27017f24e69143
Rik Visser <rik.visser@philips.com>
2021-01-07 11:42:44 +0100

    Change filename to chmod

--- containter_host.tf
+++ containter_host.tf
@@ -76,7 +76,7 @@ resource "null_resource" "cluster" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /home/${var.user}/bootstrap-cluster.sh",
-      "chmod 777 /home/${var.user}/jmxconfig.yml",
+      "chmod 777 /home/${var.user}/jmxconfig.yml.tmpl",
       "/home/${var.user}/bootstrap-cluster.sh -n ${join(",", hsdp_container_host.zookeeper.*.private_ip)} -c ${random_id.id.hex} -d ${var.image} -i ${count.index + 1} -t ${var.trust_store.password} -k ${var.key_store.password}"
     ]
   }
